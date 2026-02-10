7c200882e6b37ffba17de970afe73957aa429617
elenee <elenerainauli@gmail.com>
2026-02-10 15:52:54 +0400

    add homepage

---
+++ Project/src/main/java/pages/HomePage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage{
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+    private By signupLoginButton = By.cssSelector("a[href='/login']");
+    public boolean isHomePageVisible() {
+        return driver.getTitle().contains("Automation Exercise");
+    }
+    public HomePage clickSignupLogin() {
+        click(signupLoginButton);
+        return this;
+    }
+}
