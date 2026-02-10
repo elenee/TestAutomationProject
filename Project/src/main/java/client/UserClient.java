444dfbe0addb75b6d91aefa4568019f01aa70d24
elenee <elenerainauli@gmail.com>
2026-02-11 02:20:52 +0400

    feat: implement user deletion test

--- Project/src/main/java/client/UserClient.java
+++ Project/src/main/java/client/UserClient.java
@@ -5,12 +5,21 @@ import utils.RequestSpecFactory;
 import static io.restassured.RestAssured.given;
 
 public class UserClient {
-
     public Response getUserDetail(String email) {
         return given()
                 .spec(RequestSpecFactory.defaultJsonSpec())
                 .queryParam("email", email)
                 .when()
                 .get("/getUserDetailByEmail");
+    }
+    public Response deleteUserAccount(String email, String password) {
+        return given()
+                .spec(RequestSpecFactory.defaultJsonSpec())
+                .contentType(io.restassured.http.ContentType.URLENC)
+                .formParam("email", email)
+                .formParam("password", password)
+                .log().parameters()
+                .when()
+                .delete("/deleteAccount");
     }
 }
