5dbd6c8e84b3784f070df3c3cd297a1b75d43062
elenee <elenerainauli@gmail.com>
2026-02-10 19:59:32 +0400

    fix conflicts

--- Project/src/main/java/pages/LoginPage.java
+++ Project/src/main/java/pages/LoginPage.java
@@ -3,35 +3,56 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
     public LoginPage(WebDriver driver) {
         super(driver);
     }
-
+    private By signupNameField = By.cssSelector("input[data-qa='signup-name']");
+    private By signupEmailField = By.cssSelector("input[data-qa='signup-email']");
+    private By signupButton = By.cssSelector("button[data-qa='signup-button']");
+    private By loginEmailField = By.cssSelector("input[data-qa='login-email']");
+    private By loginPasswordField = By.cssSelector("input[data-qa='login-password']");
+    private By loginButton = By.cssSelector("button[data-qa='login-button']");
     private By loginHeader = By.xpath("//h2[text()='Login to your account']");
-    private By emailInput = By.name("email");
-    private By passwordInput = By.name("password");
-    private By loginButton = By.xpath("//button[text()='Login']");
     private By loggedInAsText = By.xpath("//a[contains(text(),'Logged in as')]");
-    private By logoutButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
+    private By logoutButton = By.xpath("//a[contains(@href, '/logout')]");
+
+    public LoginPage enterSignupName(String name) {
+        type(signupNameField, name);
+        return this;
+    }
+
+    public LoginPage enterSignupEmail(String email) {
+        type(signupEmailField, email);
+        return this;
+    }
+
+    public SignupPage clickSignupButton() {
+        click(signupButton);
+        return new SignupPage(driver);
+    }
+
+    public LoginPage enterLoginEmail(String email) {
+        type(loginEmailField, email);
+        return this;
+    }
+
+    public LoginPage enterLoginPassword(String password) {
+        type(loginPasswordField, password);
+        return this;
+    }
+
+    public HomePage clickLogin() {
+        click(loginButton);
+        return new HomePage(driver);
+    }
 
     public boolean isLoginHeaderVisible() {
         return isDisplayed(loginHeader);
     }
 
-    public LoginPage enterEmail(String email) {
-        type(emailInput, email);
-        return this;
-    }
-
-    public LoginPage enterPassword(String password) {
-        type(passwordInput, password);
-        return this;
-    }
-
-    public LoginPage clickLogin() {
-        click(loginButton);
-        return this;
+    public boolean isLoginPageVisible() {
+        return isDisplayed(loginButton);
     }
 
     public boolean isLoggedInAsVisible() {
@@ -42,10 +63,4 @@ public class LoginPage extends BasePage{
         click(logoutButton);
         return new HomePage(driver);
     }
-}
-    public LoginPage clickLogout() {
-        click(logoutButton);
-        return this;
-    }
-
 }
